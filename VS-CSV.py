473b5af1877d6c14c1e7492f21c4986f22e88fa4
KSitrin <sitrin1@kenyon.edu>
2025-04-03 15:22:33 -0400

    Update VS-CSV.py
    - Noted html next steps

--- VS-CSV.py
+++ VS-CSV.py
@@ -1,92 +1,57 @@
-import csv
-#import CSV file
-# copied from https://automatetheboringstuff.com/chapter14/
-def process_csv(filename):
-    exampleFile = open(filename, encoding="utf-8")
-    exampleReader = csv.reader(exampleFile)
-    exampleData = list(exampleReader)
-    exampleFile.close()
-    return exampleData
 
-csvFilepath="Tester.csv"
-htmlFilepath="KCHS Image Display.html"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>[Photo Title]-[Date Captured]</title>
+    <style>
+        body {
+            font-family: Geneva, sans-serif;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+            background-color: #091831;
+        }
+	.logo {
+	    max-width: 500px;
+            margin: 50px auto;
+            padding: 20px;
+            background-color: #ECF2FD;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+	}
+        .container {
+            max-width: 1200px;
+            margin: 50px auto;
+            padding: 20px;
+            background-color: #ECF2FD;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        img {
+            max-width: 100%px;
+            height: auto;
+            border-radius: 8px;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            font-size: 1.1em;
+            color: #555;
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<div class="logo">
+<a href='https://www.knoxhistory.org' target='_blank'><img src='https://www.knoxhistory.org/templates/rt_studius/custom/images/logo.png' border='0' alt='Knox History Logo'/></a>
+<body>
+</div>
+<div class="container">
+    <h1>[Photo Title]</h1>
+    <h3>[Date Captured]</h3>
+    <p>[Description]</p>
+    <img src="[Link]" alt="[Image Title + ID]">
+</div>
 
-MyDB=process_csv(csvFilepath)
-print(MyDB)
-
-def dbCategories(file_path):
-    try:
-        with open(file_path, mode='r', newline='') as file:
-            csv_reader = csv.reader(file)
-            # Read the first row (or list) from the CSV file and return it
-            categories = next(csv_reader)
-            return categories
-    except FileNotFoundError:
-        print(f"File at {file_path} not found.")
-    except Exception as e:
-        print(f"Error: {e}")
-        return []
-
-
-# Function to add a new row to the CSV file
-def add_row_to_csv(file_path):
-    columns=dbCategories(file_path)
-    # Open the CSV file in append mode
-    addedRows=int(input("How many new items are you adding?"))
-    for i in range(addedRows):
-        with open(file_path, mode='a', newline='') as file:
-            csv_writer = csv.writer(file)
-            row=[]
-            for j in columns:
-                entry = input(f"Enter your image's {j}: ")
-                # Convert the input string into a list by splitting by commas
-                row.append(entry.strip())
-            print(row)
-            # Write the new row to the CSV file
-            csv_writer.writerow(row)
-            print("New photo added successfully!")
-
-# Function to remove a row from CSV file based on ID
-def remove_row_from_csv(input_file):
-    removedID=input("Input the photo IDs you want to remove, separating each with a comma:  ")
-    #creates list of IDs
-    removedID.split(",")
-    rows_to_keep = []
-    with open(input_file, mode='r', newline='') as infile:
-        reader = csv.reader(infile)
-        for row in reader:
-            # Check if this row matches the ID and should be removed
-            if row[0] not in removedID:
-                rows_to_keep.append(row)
-    # Rewrite CSV file with the filtered rows
-    with open(input_file, mode='w', newline='') as outfile:
-        writer = csv.writer(outfile)
-        writer.writerows(rows_to_keep)
-    print("Photos removed successfully!")
-
-#function to create html site to display photo and information
-def createHTMLforPhoto(HTMLbase,picTitle,picDate,picDesc,picImgLink,picID):
-    oldHTML= open(HTMLbase, 'r', encoding='utf-8')
-    newtext = oldHTML.read()
-    newtext=newtext.replace("[Photo Title]",picTitle)
-    newtext=newtext.replace("[Date Captured]",picDate)
-    newtext=newtext.replace("[Description]", picDesc)
-    newtext=newtext.replace("[Link]",picImgLink)
-    newtext=newtext.replace("[Image Title + ID]",str(picTitle+picID))
-    oldHTML.close()
-    newHTML=open(str("KCHS-images-"+picTitle+"-"+picID+".html"),'w',encoding='utf-8')
-    newHTML.write(newtext)
-    newHTML.close()
-    print("done!")
-
-
-testing=input("\n \n Input 1 to add a row to the csv, 2 to remove, and 3 to create an html.  ")
-if testing=="1":
-    add_row_to_csv(csvFilepath)
-elif testing=="2":
-    remove_row_from_csv(csvFilepath)
-elif testing=="3":
-    createHTMLforPhoto(htmlFilepath,"Testing","03/18/25","This is a testing description.","https://www.knoxhistory.org/templates/rt_studius/custom/images/headers/81623446_2819543581605978_5690387876819238912_n.jpg","12345")
-else:
-    print("No effect, try again.")
-    testing=input("\n \n Input 1 to add a row to the csv, 2 to remove, and 3 to create an html.  ")
+</body>
+</html>
