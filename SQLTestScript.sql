2ddb47d97d91869b07cfc129ca972cf8ff6bf879
4934771143611516286 <weber4@kenyon.edu>
2025-04-14 21:40:12 -0400

    Add files via upload

---
+++ SQLTestScript.sql
@@ -0,0 +1,19 @@
+USE importtestdb;
+CREATE TABLE IF NOT EXISTS imageimporttable(
+	ID INTEGER,
+    dateTaken DATE,
+    Title TEXT,
+    Link TEXT,
+    Descript TEXT,
+    dateAdded DATE,
+    PRIMARY KEY (ID)
+);
+
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ImportTester.csv'
+INTO TABLE imageimporttable
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM imageimporttable;
