0d0e777f05ec536cde96046e9e83bf0e96dcdbbd
4934771143611516286 <weber4@kenyon.edu>
2025-04-25 13:17:41 -0400

    Update SQLTestScript.sql
    Remove unnecessary code.

--- SQLTestScript.sql
+++ SQLTestScript.sql
@@ -8,12 +8,4 @@ CREATE TABLE IF NOT EXISTS imageimporttable(
     dateAdded TEXT,
     PRIMARY KEY (ID)
 );
-
-LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ImportTester.csv'
-INTO TABLE imageimporttable
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
 SELECT * FROM imageimporttable;
