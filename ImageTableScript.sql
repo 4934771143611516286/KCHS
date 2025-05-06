b4e0acb4665aded5e526d5a1bcffa71f37cc9eba
4934771143611516286 <weber4@kenyon.edu>
2025-05-05 22:23:57 -0400

    Update ImageTableScript.sql

--- ImageTableScript.sql
+++ ImageTableScript.sql
@@ -19,3 +19,5 @@ INSERT INTO users (username, passcode)
 VALUES ('Username','Passphrase'); -- Switch these with the username and password of the new user.
 
 SELECT * FROM users; -- Displays all user information
+
+DELETE FROM users WHERE id = 0; -- Run this line if you want to delete an entry where the login information is wrong, just replace the 0 with the id you want to delete. 
